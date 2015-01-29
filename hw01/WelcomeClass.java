b07da11b53d7865ce98c0f8ef9cefd795f2c9337
alyssagallagher <aag218@lehigh.edu>
2015-01-29 01:01:26 +0000

    Updated hw01

---
+++ hw01/WelcomeClass.java
@@ -0,0 +1,29 @@
+// Alyssa Gallagher
+// Welcome Class Java Program
+//
+//  first compile the program
+//      javac WelcomeClass.java
+// run the program
+//      java Welcome Class//
+
+// define a class
+public class WelcomeClass{
+
+// add main method
+public static void main(String[] args){
+    
+// print the statement
+    System.out.println("   -----------  ");
+    System.out.println("   | WELCOME |  ");
+    System.out.println("   -----------  ");
+    System.out.println("   ^ ^ ^ ^ ^ ^  ");
+    System.out.println("/ \\/ \\/ \\/ \\/ \\/ \\ ");
+    System.out.println("<-A--A--G--2--1--8->");
+    System.out.println(" \\ /\\ /\\ /\\ /\\ /\\ /");
+    System.out.println("  v  v  v  v  v  v");
+    System.out.println(" I live in Medford, New Jersey on 63 acres of land with my two older siblings and parents. Chickens, two dogs, a cat and various other wildlife species inhabit our land.");
+    
+    //test/testtest
+    
+    }
+} 
